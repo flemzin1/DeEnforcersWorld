cbc5ecd639b37542780e3a5e088393a70aadbe80
Flemzzin1 <flemzin1@gmail.com>
2025-07-12 19:55:38 +0100

    noIndex commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Homepage from './pages/Homepage';
@@ -9,35 +10,50 @@ import Give from './pages/Give';
 import SatelliteCenters from './pages/SateliteCenters';
 import OurFounder from './pages/OurFounder';
 import Organograms from './pages/Organograms';
-import ChurchEvents from './pages/EventsPage'; 
+import ChurchEvents from './pages/EventsPage';
 import OutreachPage from './pages/Outreach';
 import ChurchBlog from './pages/Blogs';
 import './App.css';
 
-const App: React.FC = () => {
+const AppContent: React.FC = () => {
+  const location = useLocation();
+  const [canonicalUrl, setCanonicalUrl] = useState('');
+
+  useEffect(() => {
+    setCanonicalUrl(`https://deenforcersworld.com${location.pathname}`);
+  }, [location]);
+
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/founder" element={<OurFounder />} />
-            <Route path="/organograms" element={<Organograms />} />
-            {/* <Route path="/ministries" element={<Ministries />} /> */}
-            <Route path="/events" element={<ChurchEvents />} />
-            <Route path="/outreach" element={<OutreachPage />} />
-            <Route path="/blogs" element={<ChurchBlog />} />
-            <Route path="/messages" element={<Messages />} />
-            <Route path="/give" element={<Give />} />
-            <Route path="/satellite-centers" element={<SatelliteCenters />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <>
+      <Helmet>
+        <link rel="canonical" href={canonicalUrl} />
+      </Helmet>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/founder" element={<OurFounder />} />
+          <Route path="/organograms" element={<Organograms />} />
+          {/* <Route path="/ministries" element={<Ministries />} /> */}
+          <Route path="/events" element={<ChurchEvents />} />
+          <Route path="/outreach" element={<OutreachPage />} />
+          <Route path="/blogs" element={<ChurchBlog />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/give" element={<Give />} />
+          <Route path="/satellite-centers" element={<SatelliteCenters />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
   );
 };
+
+
+const App: React.FC = () => (
+  <Router>
+    <AppContent />
+  </Router>
+);
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import flowbiteReact from 'flowbite-react/plugin/vite';
 
 export default defineConfig({
   plugins: [react(), flowbiteReact()],
+  ssr: {
+    noExternal: ['react-helmet-async'],
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
